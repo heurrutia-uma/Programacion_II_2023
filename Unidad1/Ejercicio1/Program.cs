7326230d600a0bdacdfd7a19ec4c389837d523b6
DESKTOP-IH38QMP\Admin <hector.urrutia@uma.edu.sv>
2023-01-20 18:51:24 -0600

    Unidad 1 - Ejercicio1

---
+++ Unidad1/Ejercicio1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("¡Bienvenido a ");
+            Console.WriteLine("Programación II!");
+            Console.Write("Ingrese su nombre: ");
+
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Buen día, {0}!", nombre);
+
+            Console.WriteLine("\nPulse Escape (Esc) para salir.");
+            while (Console.ReadKey().Key != ConsoleKey.Escape) { }
+        }
+    }
+}
