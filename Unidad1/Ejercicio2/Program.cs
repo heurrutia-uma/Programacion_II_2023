903adc087bdfb6f86cc03ad1360e06f342ae44ff
DESKTOP-IH38QMP\Admin <hector.urrutia@uma.edu.sv>
2023-01-20 19:02:44 -0600

    Unidad1 - Ejercicio 2

---
+++ Unidad1/Ejercicio2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int primerNumero, segundoNumero;
+
+            Console.Write("Introduce el primer número: ");
+            primerNumero = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Introduce el segundo número: ");
+            segundoNumero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Los números ingresados son: {0} y {1}.", primerNumero, segundoNumero );
+
+            Console.WriteLine("\nPulse Escape (Esc) para salir.");
+            while (Console.ReadKey().Key != ConsoleKey.Escape) { }
+        }
+    }
+}
