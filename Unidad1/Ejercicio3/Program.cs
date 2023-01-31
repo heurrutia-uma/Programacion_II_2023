ca7a70f6e0a61c61ac11a2ec8121261928fa4bc3
DESKTOP-IH38QMP\Admin <hector.urrutia@uma.edu.sv>
2023-01-30 21:26:54 -0600

    Ejercicio3 28-01-2023

---
+++ Unidad1/Ejercicio3/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ejercicio3
+{
+    internal class Program
+    {
+        static string archivo;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sistema Registro de Clientes.");
+            Console.Write("Ingrese ruta y nombre de archivo: ");
+            archivo = CrearAbrirArchivo(Console.ReadLine());
+            Menu();
+        }
+
+        static void Menu()
+        {
+            if (!string.IsNullOrEmpty(archivo))
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("Ruta o nombre de achivo vacio o inválido.\nPulse Escape [Esc] para salir.");
+                while (true) 
+                {
+                    if (Console.ReadKey().Key == ConsoleKey.Escape)
+                        break;
+                }
+            }
+        }
+
+        static string CrearAbrirArchivo(string archivo)
+        {
+            string archivoValido = "";
+
+            if (!String.IsNullOrEmpty(archivo))
+            {
+                string rutaArchivo = Path.GetDirectoryName(archivo);
+                string nombreArchivo = Path.GetFileName(archivo);
+
+                if (!String.IsNullOrEmpty(rutaArchivo))
+                {
+                    if (!Directory.Exists(rutaArchivo))
+                        Directory.CreateDirectory(rutaArchivo);
+                }
+                else
+                    rutaArchivo = Directory.GetCurrentDirectory();
+
+
+                if (!File.Exists(Path.Combine(rutaArchivo, nombreArchivo)))
+                {
+                    File.Create(Path.Combine(rutaArchivo, nombreArchivo)).Close();
+                    archivoValido = Path.Combine(rutaArchivo, nombreArchivo);
+                }
+                else
+                    archivoValido = Path.Combine(rutaArchivo, nombreArchivo);
+            }
+
+            return archivoValido;
+        }
+
+        static void VisualizarLista()
+        { }
+
+        static void AgregarRegistro()
+        { }
+
+        static void EliminarRegistro()
+        { }
+
+    }
+}
