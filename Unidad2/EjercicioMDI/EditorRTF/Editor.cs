41a90c46d93930759b20b762d3df5ad83675c161
DESKTOP-IH38QMP\Admin <hector.urrutia@uma.edu.sv>
2023-03-16 23:14:20 -0600

    Ejercicio MDI

---
+++ Unidad2/EjercicioMDI/EditorRTF/Editor.cs
@@ -0,0 +1,232 @@
+﻿using EditorRTF.Funciones;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace EditorRTF
+{
+    public partial class Editor : Form
+    {
+        public string Archivo { get; set; }
+
+        public Editor()
+        {
+            InitializeComponent();
+        }
+
+        private void Editor_Load(object sender, EventArgs e)
+        {
+            Fuente fuentes = Funciones.Funciones.CargarFamiliaFuente();
+
+            foreach (string fuente in fuentes.Nombres)
+                ComboFuente.Items.Add(fuente);
+
+            foreach (string tamanio in fuentes.Tamanios)
+                ComboTamanio.Items.Add(tamanio);
+
+            ComboFuente.SelectedItem = ContenedorRFT.Font.Name;
+            ComboTamanio.SelectedItem = Convert.ToInt32(ContenedorRFT.Font.Size).ToString();
+
+            AbrirArchivo();
+        }
+
+        private void AbrirArchivo()
+        {
+            if (!String.IsNullOrEmpty(this.Archivo))
+            {
+                ContenedorRFT.LoadFile(this.Archivo);
+            }
+        }
+
+        private void BotonGuardar_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(this.Archivo))
+            {
+                string archivo = Funciones.Dialogos.GuardarArchivoComo();
+
+                if (!String.IsNullOrEmpty(archivo))
+                {
+                    this.Archivo = archivo;
+                    ContenedorRFT.SaveFile(this.Archivo);
+                }
+            }
+            else
+            {
+                ContenedorRFT.SaveFile(this.Archivo);
+            }
+        }
+
+        private void BotonSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void AplicarEstilo(FontStyle estilo)
+        {
+            // Recuperar valores de estilo de fuente y tamaño desde los controles combobox.
+            string fuenteCombo = ComboFuente.SelectedItem.ToString();
+            float tamanioCombo;
+
+            if (ComboTamanio.SelectedItem == null)
+                tamanioCombo = (float)Convert.ToDecimal(Funciones.Funciones.FUENTE_TAMANIO_INICIAL);
+            else
+                tamanioCombo = (float)Convert.ToDecimal(ComboTamanio.SelectedItem);
+
+            // Verificamos que fuente y tamaño aplicar
+            string fuente;
+            float tamanio;
+
+            if (ContenedorRFT.SelectionFont != null)
+            {
+                string fuenteSeleccionada = ContenedorRFT.SelectionFont.Name;
+                float tamanioSeleccionada = ContenedorRFT.SelectionFont.Size;
+
+                if (fuenteCombo != fuenteSeleccionada)
+                    fuente = fuenteCombo;
+                else
+                    fuente = fuenteSeleccionada;
+
+                if (tamanioCombo != tamanioSeleccionada)
+                    tamanio = tamanioCombo;
+                else
+                    tamanio = tamanioSeleccionada;
+            }
+            else
+            {
+                fuente = fuenteCombo;
+                tamanio = tamanioCombo;
+            }
+
+            // Aplicamos fuente, tamaño y estilo
+            ContenedorRFT.SelectionFont = new Font(fuente, tamanio, estilo);
+        }
+
+        private void ComboFuente_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FontStyle estilo;
+
+            if (ContenedorRFT.SelectionFont == null)
+                estilo = FontStyle.Regular;
+            else
+                estilo = ContenedorRFT.SelectionFont.Style;
+
+            AplicarEstilo(estilo);
+        }
+
+        private void ComboTamanio_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FontStyle estilo;
+
+            if (ContenedorRFT.SelectionFont == null)
+                estilo = FontStyle.Regular;
+            else
+                estilo = ContenedorRFT.SelectionFont.Style;
+
+            AplicarEstilo(estilo);
+        }
+
+        private void BotonNegrita_Click(object sender, EventArgs e)
+        {
+            FontStyle estilo = FontStyle.Bold;
+
+            if (ContenedorRFT.SelectionFont != null)
+            {
+                estilo = ContenedorRFT.SelectionFont.Style;
+
+                if (ContenedorRFT.SelectionFont.Bold)
+                    estilo &= ~FontStyle.Bold;
+                else
+                    estilo |= FontStyle.Bold;
+            }
+
+            AplicarEstilo(estilo);
+        }
+
+        private void BotonItalica_Click(object sender, EventArgs e)
+        {
+            FontStyle estilo = FontStyle.Italic;
+
+            if (ContenedorRFT.SelectionFont != null)
+            {
+                estilo = ContenedorRFT.SelectionFont.Style;
+
+                if (ContenedorRFT.SelectionFont.Italic)
+                    estilo &= ~FontStyle.Italic;
+                else
+                    estilo |= FontStyle.Italic;
+            }
+
+            AplicarEstilo(estilo);
+        }
+
+        private void BotonSubrayado_Click(object sender, EventArgs e)
+        {
+            FontStyle estilo = FontStyle.Underline;
+
+            if (ContenedorRFT.SelectionFont != null)
+            {
+                estilo = ContenedorRFT.SelectionFont.Style;
+
+                if (ContenedorRFT.SelectionFont.Underline)
+                    estilo &= ~FontStyle.Underline;
+                else
+                    estilo |= FontStyle.Underline;
+            }
+
+            AplicarEstilo(estilo);
+        }
+
+        private void BotonColor_Click(object sender, EventArgs e)
+        {
+            Color colorFuente = Funciones.Dialogos.SeleccionarColorFuente();
+
+            if (!colorFuente.IsEmpty && ContenedorRFT.SelectionFont != null)
+                ContenedorRFT.SelectionColor = colorFuente;
+        }
+
+        private void BotonIzquierda_Click(object sender, EventArgs e)
+        {
+            ContenedorRFT.SelectionAlignment = HorizontalAlignment.Left;
+        }
+
+        private void BotonCentro_Click(object sender, EventArgs e)
+        {
+            ContenedorRFT.SelectionAlignment = HorizontalAlignment.Center;
+        }
+
+        private void BotonDerecha_Click(object sender, EventArgs e)
+        {
+            ContenedorRFT.SelectionAlignment = HorizontalAlignment.Right;
+        }
+
+        private void CortarMenuItem_Click(object sender, EventArgs e)
+        {
+            ContenedorRFT.Cut();
+        }
+
+        private void CopiarMenuItem_Click(object sender, EventArgs e)
+        {
+            ContenedorRFT.Copy();
+        }
+
+        private void PegarMenuItem_Click(object sender, EventArgs e)
+        {
+            if (ContenedorRFT.CanPaste(DataFormats.GetFormat(DataFormats.Text)))
+                ContenedorRFT.Paste();
+        }
+
+        private void DeshacerMenuItem_Click(object sender, EventArgs e)
+        {
+            if (ContenedorRFT.CanUndo)
+                ContenedorRFT.Undo();
+        }
+
+        private void rehacerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (ContenedorRFT.CanRedo)
+                ContenedorRFT.Redo();
+        }
+    }
+
+}
---
+++ Unidad2/EjercicioMDI/EditorRTF/FormPrincipal.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace EditorRTF
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MenuPrincipal = new System.Windows.Forms.MenuStrip();
+            this.MenuArchivo = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemNuevo = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemAbrir = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.MenuItemSalir = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuPrincipal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MenuPrincipal
+            // 
+            this.MenuPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuArchivo});
+            this.MenuPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.MenuPrincipal.Name = "MenuPrincipal";
+            this.MenuPrincipal.Size = new System.Drawing.Size(800, 24);
+            this.MenuPrincipal.TabIndex = 1;
+            this.MenuPrincipal.Text = "menuStrip1";
+            // 
+            // MenuArchivo
+            // 
+            this.MenuArchivo.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuItemNuevo,
+            this.MenuItemAbrir,
+            this.toolStripSeparator1,
+            this.MenuItemSalir});
+            this.MenuArchivo.Name = "MenuArchivo";
+            this.MenuArchivo.Size = new System.Drawing.Size(60, 20);
+            this.MenuArchivo.Text = "&Archivo";
+            // 
+            // MenuItemNuevo
+            // 
+            this.MenuItemNuevo.Image = global::EditorRTF.Properties.Resources.NewFile_16x;
+            this.MenuItemNuevo.Name = "MenuItemNuevo";
+            this.MenuItemNuevo.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.MenuItemNuevo.Size = new System.Drawing.Size(180, 22);
+            this.MenuItemNuevo.Text = "&Nuevo";
+            this.MenuItemNuevo.Click += new System.EventHandler(this.MenuItemNuevo_Click);
+            // 
+            // MenuItemAbrir
+            // 
+            this.MenuItemAbrir.Image = global::EditorRTF.Properties.Resources.OpenFile_16x;
+            this.MenuItemAbrir.Name = "MenuItemAbrir";
+            this.MenuItemAbrir.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.A)));
+            this.MenuItemAbrir.Size = new System.Drawing.Size(180, 22);
+            this.MenuItemAbrir.Text = "A&brir";
+            this.MenuItemAbrir.Click += new System.EventHandler(this.MenuItemAbrir_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // MenuItemSalir
+            // 
+            this.MenuItemSalir.Image = global::EditorRTF.Properties.Resources.Close_16x;
+            this.MenuItemSalir.Name = "MenuItemSalir";
+            this.MenuItemSalir.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.X)));
+            this.MenuItemSalir.Size = new System.Drawing.Size(180, 22);
+            this.MenuItemSalir.Text = "&Salir";
+            this.MenuItemSalir.Click += new System.EventHandler(this.MenuItemSalir_Click);
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MenuPrincipal);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.MenuPrincipal;
+            this.Name = "FormPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EditorRTF";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.MenuPrincipal.ResumeLayout(false);
+            this.MenuPrincipal.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip MenuPrincipal;
+        private System.Windows.Forms.ToolStripMenuItem MenuArchivo;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemNuevo;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemAbrir;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemSalir;
+    }
+}
